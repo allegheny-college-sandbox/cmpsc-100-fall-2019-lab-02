175f952c5315fd7260eab31bce65ceb6298e023a
Douglas Luman <dluman@douglasjluman.com>
2019-09-08 09:53:56 -0400

    Testing new GatorGrader grade file.

--- src/main/java/poem/DisplayPoem.java
+++ src/main/java/poem/DisplayPoem.java
@@ -1,8 +1,8 @@
 package Poem;
 
-public class DisplayOutput {
+public class DisplayPoem {
     // TODO Replace with multi-line comment.
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
