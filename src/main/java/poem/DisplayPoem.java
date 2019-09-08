dd8c23c81a298c24a1310eab7e51e671c1e756fc
Douglas Luman <dluman@douglasjluman.com>
2019-09-08 10:32:05 -0400

    Let's make the assignment work, shall we?

--- src/main/java/poem/DisplayPoem.java
+++ src/main/java/poem/DisplayPoem.java
@@ -8,15 +8,5 @@ public class DisplayPoem {
    */
   public static void main(String[] args) {
     // TODO Replace with multi-line comment.
-    System.out.println("1");
-    System.out.println("2");
-    System.out.println("3");
-    System.out.println("4");
-    System.out.println("5");
-    System.out.println("6");
-    System.out.println("7");
-    System.out.println("8");
-    System.out.println("9");
-    System.out.println("10");
   }
 }
