479d239880963e3c74a39b33a81a082c2aee39f9
Douglas Luman <dluman@douglasjluman.com>
2019-09-08 10:15:24 -0400

    Must remember that Google like two-space indents.

--- src/main/java/poem/DisplayPoem.java
+++ src/main/java/poem/DisplayPoem.java
@@ -1,17 +1,17 @@
 package Poem;
 
 public class DisplayPoem {
-    // TODO Replace with multi-line comment.
-    public static void main(String[] args) {
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
-        System.out.println("5");
-        System.out.println("6");
-        System.out.println("7");
-        System.out.println("8");
-        System.out.println("9");
-        System.out.println("10");
-    }
+  // TODO Replace with multi-line comment.
+  public static void main(String[] args) {
+    System.out.println("1");
+    System.out.println("2");
+    System.out.println("3");
+    System.out.println("4");
+    System.out.println("5");
+    System.out.println("6");
+    System.out.println("7");
+    System.out.println("8");
+    System.out.println("9");
+    System.out.println("10");
+  }
 }
